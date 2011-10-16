f0cf487169c93db21b576e89d9d88c5b71058fd8
Matija Korpar <mkorpar@gmail.com>
2011-10-16 15:22:51 +0200

    Licence

--- src/matrices/CHIAR.h
+++ src/matrices/CHIAR.h
@@ -1,3 +1,23 @@
+/*
+SW# - CUDA parallelized Smith Waterman with applying Hirschberg's algorithm
+Copyright (C) 2011 Matija Korpar
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Contact the author by mkorpar@gmail.com.
+*/
+
 #ifndef __CHIARH__
 #define __CHIARH__
 
